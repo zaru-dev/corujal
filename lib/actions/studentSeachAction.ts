0a1ae3aa67cd391e5f5aedac9e0f69856e21bc57
Raave Aires <dev@raave.me>
2025-06-27 21:43:31 -0300

    feat(actions): adiciona ação de servidor para busca de aluno

---
+++ lib/actions/studentSeachAction.ts
@@ -0,0 +1,54 @@
+"use server";
+
+// funções:
+import { db } from "@/lib/db";
+
+interface SearchStudentActionResult {
+  success: boolean;
+  message?: string;
+  dados?: {
+    matricula: string,
+    aluno: string,
+    turma: string,
+    turno: string,
+  }
+  errors?: {
+    [key: string]: string[] | undefined;
+  };
+}
+
+export async function seachStudent(
+  _prevState: any,
+  data: {
+    matricula: string
+  }
+): Promise<SearchStudentActionResult> {
+  try {
+    // verifica se o aluno está cadastrado no banco de dados:
+    const student = await db.aluno.findUnique({
+      where: { matricula: data.matricula },
+    });
+
+    if (!student) {
+      return {
+        success: false,
+        message: "Este(a) aluno(a) não está cadastado(a)"
+      }
+    };
+
+    return {
+      success: true,
+      dados: {
+        matricula: student.matricula,
+        aluno: student.aluno,
+        turma: student.turma,
+        turno: student.turno
+      }
+    };
+  } catch(error) {
+    return {
+      success: false,
+      message: "Ocorreu um erro inesperado. Tente novamente."
+    }
+  }
+}
