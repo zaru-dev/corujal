00711f191dc01f4b21a34f247a500508df11d768
Raave Aires <dev@raave.me>
2025-07-05 13:02:30 -0300

    feat: adiciona menu suspenso com opções de ação no componente OccurrenceCard

--- components/occurrenceCard.tsx
+++ components/occurrenceCard.tsx
@@ -1,3 +1,14 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Copy, Download, Ellipsis, Pencil, Trash2 } from "lucide-react";
+import { Button } from "./ui/button";
+
 interface OccurrenceCardProps {
   codigo: string;
   descricao: string;
@@ -5,21 +16,43 @@ interface OccurrenceCardProps {
   medida: string;
 }
 
-export function OccurrenceCard({ codigo, descricao, data, medida }: OccurrenceCardProps) {
+export function OccurrenceCard({
+  codigo,
+  descricao,
+  data,
+  medida,
+}: OccurrenceCardProps) {
   return (
-    <div className="p-4 mt-4 border rounded-md text-sm bg-input/30">
+    <div className="p-4 border rounded-md text-sm bg-input/30 relative">
+      <div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="absolute right-3 top-3 size-7">
+              <Ellipsis />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent side="right" align="start" className="ml-4">
+            <DropdownMenuLabel>Opções</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem><Copy /> Copiar código</DropdownMenuItem>
+            <DropdownMenuItem><Pencil/> Editar</DropdownMenuItem>
+            <DropdownMenuItem><Download />Baixar em PDF</DropdownMenuItem>
+            <DropdownMenuItem variant="destructive"><Trash2/> Excluir</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
       <ul className="grid grid-cols-3 grid-rows-2 mb-2">
-        <li className="font-semibold">Ocorrência</li>
-        <li className="font-semibold">Data</li>
-        <li className="font-semibold">Medida</li>
+        <li className="font-semibold select-none">Ocorrência</li>
+        <li className="font-semibold select-none">Data</li>
+        <li className="font-semibold select-none">Medida</li>
 
         <li>{codigo}</li>
         <li>{data}</li>
-        <li>{medida}</li>
+        <li>{medida === "Advertência verbal" ? "Adv. verbal" : medida === "Advertência escrita" ? "Adv. escrita" : medida }</li>
       </ul>
 
       <div className="flex flex-col">
-        <p className="font-semibold">Detalhes</p>
+        <p className="font-semibold select-none">Detalhes</p>
         <p className="hyphens-auto">{descricao}</p>
       </div>
     </div>
