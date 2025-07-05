886ce7d2955445eecfc1587422e5685d4cf85dd7
Raave Aires <dev@raave.me>
2025-07-05 13:42:46 -0300

    fix: adiciona suporte para classe personalizada no componente Feedback

--- components/feedback.tsx
+++ components/feedback.tsx
@@ -3,11 +3,12 @@ import { ReactNode } from "react";
 interface FeedbackProps {
   icon: ReactNode;
   text: string;
+  className?: string;
 }
 
-export function Feedback({ icon, text }: FeedbackProps) {
+export function Feedback({ icon, text, className }: FeedbackProps) {
   return (
-    <div className="flex gap-2 items-center p-2 border rounded-md text-sm">
+    <div className={`flex gap-2 items-center p-2 border rounded-md text-sm ${className}`}>
       {icon} <p>{text}</p>
     </div>
   );
