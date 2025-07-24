38c194c31061099b7eae408c8fd8c3fd46af114b
Luiz Felipe <zaru@raavius.com>
2025-07-24 14:46:43 -0300

    Atualização de Home button

--- app/cadastrar/aluno/page.tsx
+++ app/cadastrar/aluno/page.tsx
@@ -1,16 +1,28 @@
 // Importações de componentes
+import Link from "next/link";
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { StudentRegistrationForm } from "@/components/forms/studentRegistrationForm";
 
 export default function CadastrarPage() {
   return (
-    <Card className="max-w-md w-full max-h-[calc(100dvh-2rem)] gap-0">
-      <CardHeader>
-        <CardTitle>Cadastro de aluno</CardTitle>
-        <CardDescription>Preencha os dados do aluno</CardDescription>
-      </CardHeader>
+    <div className="flex flex-col items-center gap-6 p-6">
+      {/* Botão de voltar */}
+      <Link
+        href="/"
+        className="self-start text-blue-600 hover:underline text-sm"
+      >
+        ⬅ Voltar para a Home
+      </Link>
 
-      <StudentRegistrationForm />
-    </Card>
+      {/* Card de cadastro */}
+      <Card className="max-w-md w-full max-h-[calc(100dvh-2rem)] gap-0">
+        <CardHeader>
+          <CardTitle>Cadastro de aluno</CardTitle>
+          <CardDescription>Preencha os dados do aluno</CardDescription>
+        </CardHeader>
+
+        <StudentRegistrationForm />
+      </Card>
+    </div>
   );
 }
