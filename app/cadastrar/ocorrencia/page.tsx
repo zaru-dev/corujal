063dd39bd65231c85761cfa2bbdf7b2cef9df0d3
Luiz Felipe <zaru@raavius.com>
2025-07-24 14:29:18 -0300

    Atualizar Ocorrências

--- app/cadastrar/ocorrencia/page.tsx
+++ app/cadastrar/ocorrencia/page.tsx
@@ -1,17 +1,16 @@
-// importações de componentes:
+// app/cadastrar/page.tsx
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { StudentSeachForm } from "@/components/forms/studentSearchForm";
 
-export default function Page(){
-  return(
-    <Card className="max-w-md w-full max-h-[calc(100dvh-2rem)] gap-0">
+export default function CadastrarBuscaPage() {
+  return (
+    <Card className="max-w-md w-full max-h-[calc(100dvh-2rem)] gap-0 mx-auto mt-6">
       <CardHeader>
-        <CardTitle>Cadastrar ocorrência</CardTitle>
+        <CardTitle>Cadastrar Ocorrência</CardTitle>
         <CardDescription>Digite a matrícula para começar</CardDescription>
       </CardHeader>
 
       <StudentSeachForm />
-      
     </Card>
   );
-};
+}
