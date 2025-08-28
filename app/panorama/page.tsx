01774515d665233b9eac5f29c8aee6b4ee542c9c
Raave Aires <dev@raave.me>
2025-08-28 10:52:52 -0300

    remove: remove o header da página de panorama wip[feat]: implementa um header para ser usado globalmente

--- app/panorama/page.tsx
+++ app/panorama/page.tsx
@@ -1,22 +1,10 @@
 import { RenderOccurrences } from "@/components/renderOccurrences";
-import { ThemeToggle } from "@/components/ui/theme-toggle";
-import Link from "next/link";
+import { SiteHeader } from "@/components/siteHeader";
 
 export default function Page(){
   return(
     <>
-       <header className="sticky h-14 top-0 flex items-center justify-center bg-background/95 backdrop-blur-sm supports-backdrop-filter:bg-background/60 border-b border-border z-50">
-        <nav className="container flex justify-between items-center">
-          <Link
-            href="/"
-            className="font-averia text-2xl font-semibold"
-          >
-            corujal
-          </Link>
-
-          <ThemeToggle />
-        </nav>
-      </header>
+      <SiteHeader />
 
       <section className="max-w-dvw w-full min-h-main-container my-4 flex justify-center">
         <main className="container w-full flex flex-wrap gap-4 content-start">
---
+++ components/siteHeader.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { ThemeToggle } from "./ui/theme-toggle";
+import { Button } from "./ui/button";
+import { AccountActionButton } from "./home-buttons/accountActionsButton";
+
+export function SiteHeader() {
+  return (
+    <header className="sticky h-14 top-0 flex items-center justify-center bg-background/95 backdrop-blur-sm supports-backdrop-filter:bg-background/60 border-b border-border z-50">
+      <nav className="container flex justify-between items-center">
+        <Link href="/" className="font-averia text-2xl font-semibold">
+          corujal
+        </Link>
+
+        <div className="flex gap-2 items-center">
+          <AccountActionButton action="register"/>
+          <AccountActionButton action="login"/>
+          <ThemeToggle />
+        </div>
+      </nav>
+    </header>
+  );
+}
