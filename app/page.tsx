88893bcf5e16488b6e07dba19ff14f87ec2da99d
Raave Aires <dev@raave.me>
2025-08-28 10:54:29 -0300

    wip[feat]: implementa uma home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,45 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+} from "@/components/ui/card";
+import { AccountActionButton } from "@/components/home-buttons/accountActionsButton";
+import { ActionButton } from "@/components/home-buttons/actionsButtons";
+import { SiteHeader } from "@/components/siteHeader";
+
 export default function Home() {
+  const allActions = [
+    "cadastrar-aluno",
+    "registrar-ocorrencias",
+    "ver-panorama",
+    "ver-ocorrencias",
+  ] as const;
+
   return (
-    <p>Oi</p>
+    <>
+    <SiteHeader />
+    <main className="min-h-screen min-w-screen flex justify-center items-center">
+      <Card className="border-none shadow-lg">
+        <CardHeader>
+          <CardTitle className="text-3xl md:text-4xl font-averia font-bold bg-gradient-to-r from-purple-600 to-blue-600 bg-clip-text text-transparent ">
+            corujal
+          </CardTitle>
+          <CardDescription>
+            Sistema integrado de gestão escolar.
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-2 mt-2">
+            {allActions.map((action) => (
+              <ActionButton key={action} action={action} />
+            ))}
+          </div>
+        </CardContent>
+      </Card>
+    </main>
+    </>
   );
 }
