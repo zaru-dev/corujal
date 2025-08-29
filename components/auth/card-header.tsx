44783c762b0b62bd47406440f28b3753f74eda53
Raave Aires <dev@raave.me>
2025-08-29 09:56:54 -0300

    feat: adiciona imagem adaptativa ao cabeçalho de autenticação

--- components/auth/card-header.tsx
+++ components/auth/card-header.tsx
@@ -1,5 +1,6 @@
 import { CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
+import { AdaptiveImage } from "@/components/adaptive-image";
 
 interface AuthHeaderProps {
   auth_type: "login" | "register";
@@ -9,7 +10,10 @@ export function AuthHeader({ auth_type }: AuthHeaderProps) {
   return (
     <CardHeader>
       <CardTitle>
-        <Link href="/" className="font-averia text-3xl">corujal</Link>
+        <Link href="/" className="font-averia text-3xl flex gap-2 items-center ">
+          <AdaptiveImage src_dark="https://4aol7q5h4wjpfp1m.public.blob.vercel-storage.com/corujal-branco.svg" src_light={"https://4aol7q5h4wjpfp1m.public.blob.vercel-storage.com/corujal.svg"} alt="corujal's logo" width={36} height={36} />
+          corujal
+        </Link>
       </CardTitle>
       <CardDescription>
         { auth_type === "login" ? "Bem-vindo(a) de volta. Entre para continuar." : "Para continuar, você precisa criar uma conta."}
