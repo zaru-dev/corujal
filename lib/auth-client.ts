d678800a135af7b48147d483608f07e2134ec24a
Raave Aires <dev@raave.me>
2025-08-29 20:22:53 -0300

    feat: atualiza cliente de autenticação para incluir plugins e baseURL

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,16 @@
+// funções do better-auth: 
 import { createAuthClient } from "better-auth/react"
 
+// plugins:
+import { emailOTPClient, passkeyClient, twoFactorClient } from "better-auth/client/plugins"
+
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL!
+    baseURL: process.env.BETTER_AUTH_URL!,
+    plugins: [
+        emailOTPClient(),
+        passkeyClient(),
+        twoFactorClient()
+    ]
 })
+
+export const { signIn, signUp, useSession } = createAuthClient()
